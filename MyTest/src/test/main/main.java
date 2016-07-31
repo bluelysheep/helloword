83a177bd62d44ed9cf2af43c2ab387a1aeb626ea
bluelysheep <bluelysheep@gmail.com>
2016-07-31 16:25:49 +0800

    add java file

---
+++ MyTest/src/test/main/main.java
@@ -0,0 +1,15 @@
+package test.main;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		String  test;
+		test = input.next();
+		System.out.println(test);
+	}
+
+}
